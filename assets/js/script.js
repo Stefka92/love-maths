830e8b41b1bc2d7f75153dd7bd63d4fa2cde661f
Stephanie McMahon <Smm200973@hotmail.com>
2022-12-21 23:15:12 +0000

    add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,6 +42,8 @@ if (gameType === "addition"){
   displayMultiplyQuestion(num1, num2);
 } else if (gameType ==="subtract") {
   displaySubtractQuestion(num1, num2);
+} else if (gameType ==="division") {
+  displayDivisionQuestion(num1, num2);
 } else {
   alert(`Unknown game type: ${gameType}`);
   throw `Unknown game type: ${gameType}, Aborting!`;
@@ -89,6 +91,8 @@ if (operator === "+") {
   return [operand1 * operand2, "multiply"];
 } else if (operator ==="-") {
     return [operand1 - operand2, "subtract"];
+} else if (operator ==="/") {
+  return [operand1 / operand2, "division"];
 } else {
    alert (`unimplemented operator ${operator}`)
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -132,4 +136,10 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent=operand1;
   document.getElementById('operand2').textContent=operand2;
   document.getElementById('operator').textContent="x";
+}
+function displayDivisionQuestion(operand1, operand2) {
+  operand1 = operand1 * operand2;
+  document.getElementById('operand1').textContent=operand1;
+  document.getElementById('operand2').textContent=operand2;
+  document.getElementById('operator').textContent="/";
 }
